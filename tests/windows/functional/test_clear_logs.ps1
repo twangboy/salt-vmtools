97be69e938fc6656a8c6a6d643e05d3b30264e78
Twangboy <leesh@vmware.com>
2021-11-06 18:30:47 -0600

    More fixes

--- tests/windows/functional/test_clear_logs.ps1
+++ tests/windows/functional/test_clear_logs.ps1
@@ -1,5 +1,6 @@
 # Copyright (c) 2021 VMware, Inc. All rights reserved.
 $target_dir = "$script_log_dir\spongebob"
+$target_file = "$script_log_dir\squarepants.txt"
 
 function setUpScript {
     if (!(Test-Path($script_log_dir))) {
@@ -39,27 +40,39 @@ function setUpScript {
     New-Item -Path $script_log_dir\$file_name | Out-Null
     Write-Done
 
-    Write-Host "Creating $($script_log_file_count + 2) symlinks: " -NoNewline
-    New-Item -Path "$target_dir" | Out-Null
+    Write-Host "Creating $($script_log_file_count + 2) symlinks to directory: " -NoNewline
+    New-Item -Path "$target_dir" -ItemType Directory | Out-Null
     1..$($script_log_file_count + 2) | foreach {
         $script_date = Get-Date -Format "yyyyMMddHHmmss"
-        $file_name = "$script_log_base_name-$script_date$_.log" -f "sym"
+        $file_name = "$script_log_base_name-$script_date$_.log" -f "sym-dir"
         New-Item -ItemType SymbolicLink -Path $script_log_dir\$file_name -Target $target_dir
     } | Out-Null
     Write-Done
 
+    Write-Host "Creating $($script_log_file_count + 2) symlinks to file: " -NoNewline
+    New-Item -Path "$target_file" -ItemType File | Out-Null
+    1..$($script_log_file_count + 2) | foreach {
+        $script_date = Get-Date -Format "yyyyMMddHHmmss"
+        $file_name = "$script_log_base_name-$script_date$_.log" -f "sym-file"
+        New-Item -ItemType SymbolicLink -Path $script_log_dir\$file_name -Target $target_file
+    } | Out-Null
+    Write-Done
 }
 
 function tearDownScript {
     $files = Get-ChildItem $script_log_dir -Filter "vmware-svtminion*"
     Write-Host "Removing $($files.Count) log files: " -NoNewline
     foreach ($file in $files) {
-        Remove-Item -Path $file.FullName -Force
+        Remove-FileOrFolder -Path $file.FullName
     }
     Write-Done
 
     Write-Host "Removing $($target_dir): " -NoNewline
     Remove-Item -Path $target_dir -Force
+    Write-Done
+
+    Write-Host "Removing $($target_file): " -NoNewline
+    Remove-Item -Path $target_file -Force
     Write-Done
 }
 
@@ -111,8 +124,17 @@ function test_Clear-OldLogs_more_than {
     return 1
 }
 
-function test_Clear-OldLogs_more_than_symlinks {
-    $Action = [String]$("sym")
+function test_Clear-OldLogs_more_than_symlinks_dir {
+    $Action = [String]$("sym-dir")
+    Clear-OldLogs
+    $filter = "*$script_log_base_name-*.log" -f $Action.ToLower()
+    $files = Get-ChildItem $script_log_dir -Filter $filter
+    if ($files.Count -eq $($script_log_file_count - 1)) { return 0 }
+    return 1
+}
+
+function test_Clear-OldLogs_more_than_symlinks_file {
+    $Action = [String]$("sym-file")
     Clear-OldLogs
     $filter = "*$script_log_base_name-*.log" -f $Action.ToLower()
     $files = Get-ChildItem $script_log_dir -Filter $filter
