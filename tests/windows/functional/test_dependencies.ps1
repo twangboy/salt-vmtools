f6c255968065fdc63ef4d602ccca5ae221b5b093
Twangboy <leesh@vmware.com>
2021-12-09 12:46:24 -0700

    Remove dependency on vmtoolsd.exe

--- tests/windows/functional/test_dependencies.ps1
+++ tests/windows/functional/test_dependencies.ps1
@@ -1,29 +1,33 @@
 # Copyright (c) 2021 VMware, Inc. All rights reserved.
 
-function setUp {
-    if (!(Test-Path "$vmtools_base_dir\vmtoolsd.exe")) {
-        Write-Host "Ensuring vmtoolsd present: " -NoNewline
-        New-Item -Path "$vmtools_base_path\vmtoolsd.exe" -ItemType file -Force | Out-Null
-        Write-Done
-    }
+# This test now does nothing now that the script no longer depends on
+# vmtoolsd.exe. I am leaving it in case we actually do add some dependencies in
+# the future
 
-}
-
-function tearDown {
-    if (!(Test-Path "$vmtools_base_dir\vmtoolsd.exe")) {
-        Write-Host "Ensuring vmtoolsd present: " -NoNewline
-        New-Item -Path "$vmtools_base_path\vmtoolsd.exe" -ItemType file -Force | Out-Null
-        Write-Done
-    }
-}
-
-function test_Confirm-Dependencies_all_present {
-    $result = Confirm-Dependencies
-    if ($result -eq $true) { return 0 } else { return 1 }
-}
-
-function test_Confirm-Dependencies_missing_vmtoolsd.exe {
-    Remove-Item "$vmtools_base_dir\vmtoolsd.exe"
-    $result = Confirm-Dependencies
-    if ($result -eq $false) { return 0 } else { return 1 }
-}
+#function setUp {
+#    if (!(Test-Path "$vmtools_base_dir\vmtoolsd.exe")) {
+#        Write-Host "Ensuring vmtoolsd present: " -NoNewline
+#        New-Item -Path "$vmtools_base_path\vmtoolsd.exe" -ItemType file -Force | Out-Null
+#        Write-Done
+#    }
+#
+#}
+#
+#function tearDown {
+#    if (!(Test-Path "$vmtools_base_dir\vmtoolsd.exe")) {
+#        Write-Host "Ensuring vmtoolsd present: " -NoNewline
+#        New-Item -Path "$vmtools_base_path\vmtoolsd.exe" -ItemType file -Force | Out-Null
+#        Write-Done
+#    }
+#}
+#
+#function test_Confirm-Dependencies_all_present {
+#    $result = Confirm-Dependencies
+#    if ($result -eq $true) { return 0 } else { return 1 }
+#}
+#
+#function test_Confirm-Dependencies_missing_vmtoolsd.exe {
+#    Remove-Item "$vmtools_base_dir\vmtoolsd.exe"
+#    $result = Confirm-Dependencies
+#    if ($result -eq $false) { return 0 } else { return 1 }
+#}
